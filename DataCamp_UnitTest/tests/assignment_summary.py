3faca91023a1f1a6ea371dceb71e98d0399beec5
etherion-1337 <etherion.yuki@gmail.com>
2020-02-05 17:23:02 +0800

    unit test complete

--- DataCamp_UnitTest/tests/assignment_summary.py
+++ DataCamp_UnitTest/tests/assignment_summary.py
@@ -1,10 +1,12 @@
 import pytest
 import numpy as np
 import sys
+import os
 
 sys.path.append("/Users/XavierTang/Documents/Data Science/Python/python_basics/DataCamp_UnitTest/src")
-from data.preprocessing_helpers import row_to_list, convert_to_int
+from data.preprocessing_helpers import row_to_list, convert_to_int, preprocess
 from models.train import split_into_training_and_testing_sets
+from features.as_numpy import get_data_as_numpy_array
 
 def test_for_clean_row():
 	assert row_to_list("2,081\t314,942\n") == ["2,081", "314,942"]
@@ -74,3 +76,44 @@ class TestSplitIntoTrainingAndTestingSets(object):
         with pytest.raises(ValueError) as exc_info:
             split_into_training_and_testing_sets(test_argument)
         expected_error_msg = "Argument data_array must have at least 2 rows, it actually has just 1"
+
+# # we can also use tmpdir
+# @pytest.fixture
+# def raw_and_clean_data_file(tmpdir):
+#     raw_data_file_path = tmpdir.join("raw.txt")
+#     clean_data_file_path = tmpdir.join("clean.txt")
+#     with open("raw.txt", "w") as f:
+#         f.write("1,801\t201,411\n"
+#                "1,767565,112\n"
+#                "2,002\t333,209\n"
+#                "1990\t7822,911\n"
+#                "1,285\t389129\n")
+#     yield raw_data_file_path, clean_data_file_path
+#     #no teardown needed
+
+# #test
+# def test_on_raw_data(raw_and_clean_data_file):
+#     raw_path, clean_path = raw_and_clean_data_file
+#     preprocess(raw_path, clean_path)
+#     with open(clean_data_file_path) as f:
+#         lines = f.readlines()
+#         first_line = lines[0]
+#         assert first_line == "1801\t201411\n"
+#         second_line = lines[1]
+#         assert second_line == "2002\t333209\n"
+
+# Add a decorator to make this function a fixture
+@pytest.fixture
+def clean_data_file():
+    file_path = "clean_data_file.txt"
+    with open(file_path, "w") as f:
+        f.write("201\t305671\n7892\t298140\n501\t738293\n")
+    yield file_path
+    os.remove(file_path)
+    
+# Pass the correct argument so that the test can use the fixture
+def test_on_clean_file(clean_data_file):
+    expected = np.array([[201.0, 305671.0], [7892.0, 298140.0], [501.0, 738293.0]])
+    # Pass the clean data file path yielded by the fixture as the first argument
+    actual = get_data_as_numpy_array(clean_data_file, 2)
+    assert actual == pytest.approx(expected), "Expected: {0}, Actual: {1}".format(expected, actual) 
